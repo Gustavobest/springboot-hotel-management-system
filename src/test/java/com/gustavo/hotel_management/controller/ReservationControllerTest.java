5700aa9eab4a5ecbe5dbb317d888e5c84f3bc655
Gustavo <gustavodiego550@gmail.com>
2026-07-14 15:24:47 -0500

    test: add method structure for reservartion controler test and room controller test

--- src/test/java/com/gustavo/hotel_management/controller/ReservationControllerTest.java
+++ src/test/java/com/gustavo/hotel_management/controller/ReservationControllerTest.java
@@ -1,4 +1,40 @@
 package com.gustavo.hotel_management.controller;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gustavo.hotel_management.service.ReservationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ReservationController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class ReservationControllerTest {
+
+    @Autowired
+    private MockMvc  mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private ReservationService reservationService;
+
+    @Test
+    void shouldCreateReservationSuccessFully(){}
+    @Test
+    void shouldDeleteReservationSuccessFully(){}
+    @Test
+    void shouldEditReservationSuccessFully(){}
+    @Test
+    void shouldListAllReservationSuccessFully(){}
+    @Test
+    void shouldListPaginateAllReservationSuccessFully(){}
+    @Test
+    void shouldSearchPaginateReservationSuccessFully(){}
+    @Test
+    void shouldSearchReservationForIdSuccessFully(){}
+
 }
--- src/test/java/com/gustavo/hotel_management/controller/RoomControllerTest.java
+++ src/test/java/com/gustavo/hotel_management/controller/RoomControllerTest.java
@@ -1,4 +1,46 @@
 package com.gustavo.hotel_management.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gustavo.hotel_management.service.RoomService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(RoomControllerTest.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class RoomControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private RoomService roomService;
+
+   @Test
+    void shouldCreateRoomSuccessFully(){}
+    @Test
+    void shouldEditRoomSuccessFully(){}
+    @Test
+    void shouldDeleteRoomSuccessFully(){}
+    @Test
+    void shouldListAllRoomSuccessFully(){}
+    @Test
+    void shouldListPaginatedAllRoomSuccessFully(){}
+    @Test
+    void shouldSearchPaginateRoomSuccessFully(){}
+
+
+
+
+
+
+
+
+
+
 }
